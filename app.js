2a48593e5c6896344e50c970d2e31175f80a7795
gorkem.ozbek <gorkem.ozbek@kocsistem.com.tr>
2024-07-30 23:47:02 +0300

    middleware

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use((req, res, next) => {
+    console.log(`Request: ${req.method} ${req.url}`)
+    next()
+})
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/', function (req, res) {
@@ -8,5 +15,14 @@ app.get('/', function (req, res) {
 })
 
 app.use('/users', require('./src/controllers/users'))
+
+app.use((req, res, next) => {    
+    res.status(404).send("Endpoint not found!")    
+})
+
+app.use((err, req, res, next) => {   
+    console.log(err.stack);
+    res.status(500).send('Something broke!')
+})
 
 app.listen(3000, () => console.log('App listening on port 3000!'))
