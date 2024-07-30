c3c4bbf02611029dfe4537f1a73e41c1145cb3ca
gorkem.ozbek <gorkem.ozbek@kocsistem.com.tr>
2024-07-30 22:57:00 +0300

    routing

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
-app.post('/', (req, res) => res.send('Got a POST request'))
-
-app.put('/', (req, res) => res.send('Got a PUT request'))
-
-app.delete('/', (req, res) => res.send('Got a DELETE request'))
 
 app.use('/users', require('./src/controllers/users'))
 
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/test', (req, res) => {
-  res.send('Hello World from users controller')
+router.get('/:id', (req, res) => {
+     const id = req.params.id;
+    res.send(`Get user with :id ${id}`)
+})
+
+router.get('/', (req,res) => {
+     res.send(`Get all users pageSize ${req.query.pageSize} pageNumber ${req.query.pageIndex}`)
+})
+
+router.post('/', (req,res) => {
+    res.send(`Create new user with name ${req.body.name} surname ${req.body.surname}` )
+})
+
+router.put('/', (req,res) => {
+    res.send(`Update the user with name ${req.body.name} surname ${req.body.surname}` )
+})
+
+router.delete('/', (req,res) => {
+    res.send('delete request')
 })
 
 
