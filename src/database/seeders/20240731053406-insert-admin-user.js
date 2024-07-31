df42547f12d70eb280428d8a4b2227c38cbabd70
gorkem.ozbek <gorkem.ozbek@kocsistem.com.tr>
2024-07-31 10:35:29 +0300

    seed tamamlandı

---
+++ src/database/seeders/20240731053406-insert-admin-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+                id:1,
+                username: 'admin',
+                email: 'admin@user.com',
+                password: 'pass123',
+                createdAt: new Date(),
+                updatedAt: new Date()
+              }], {})
+
+    await queryInterface.bulkInsert('Roles', [{      
+      id:1,
+      name: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }],{})
+
+    await queryInterface.bulkInsert('UserRoles', [{
+      id:1,
+      userId: 1,
+      roleId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('UserRoles', null, {});
+    await queryInterface.bulkDelete('Roles', null, {});
+    await queryInterface.bulkDelete('Users', null, {});    
+  }
+};
